10b155ea1a2631f2dc3da8092321aa0879cd0016
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-14 13:44:09 +0530

    test function for find()

---
+++ problems/test/testFind.js
@@ -0,0 +1,8 @@
+import find from "../find.js";
+import { items } from "../../dataset/dataset.js";
+const cb = (value, index) => {
+  // if (value > -1) return value; // used for testing purpose
+  /* ADD YOUR CODE HERE*/
+};
+const result = find(items, cb);
+console.log(result);
