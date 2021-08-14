082adb2bf5265321945f4353d54415bd9d2e9204
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-14 12:11:10 +0530

    modified cb function and added some comment

--- problems/test/testEach.js
+++ problems/test/testEach.js
@@ -1,6 +1,7 @@
 import each from "../each.js";
 import { items } from "../../dataset/dataset.js";
 const cb = (value, index) => {
-  console.log(value);
+  // console.log(value);   // used for testing purpose
+  /* ADD YOUR CODE HERE*/
 };
 each(items, cb);
